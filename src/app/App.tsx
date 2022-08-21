7f907c8d8e8b1869ae00d1dcd9a59ca7d4bff626
UserPasha <pasharoma13@gmail.com>
2022-08-21 20:22:31 +0300

    Profile page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,8 @@ import { Header, LinerProgress } from 'components'
 import { useSelector } from 'react-redux'
 import { selectorsIsLoading } from 'store'
 
+import { Profile } from '../pages/profile/Profile'
+
 import s from './App.module.sass'
 
 export const App = () => {
@@ -15,6 +17,7 @@ export const App = () => {
       <div className={s.linerProgressWrapper}>
         <LinerProgress isLoading={isLoading} />
       </div>
+      <Profile />
     </>
   )
 }
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,5 +1,81 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
+
+import { NavLink } from 'react-router-dom'
+
+import style from './Profile.module.sass'
+
+import { itIncubatorLogo, avatar, exitArrow, camera, pencil, logout } from './index'
 
 export const Profile = () => {
-  return <div></div>
+  const [mode, setMode] = useState<boolean>(false)
+  const [value, setValue] = useState<string>('Name')
+  const NameChanger = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value)
+  }
+
+  return (
+    <div className={style.pageWrapper}>
+      <div className={style.pageHeader}>
+        <div className={style.logo}>
+          <img src={itIncubatorLogo} alt={'logo'} />
+        </div>
+        <div className={style.info}>
+          <div className={style.infoName}>{value}</div>
+          <div className={style.infoAvatar}>
+            <img src={avatar} alt={'avatar miniature picture'} />
+          </div>
+        </div>
+      </div>
+      <div className={style.exitArrow}>
+        <NavLink to={'/'}>
+          <img src={exitArrow} alt={'arrow to exit'} />
+          <p>Back to Packs List</p>
+        </NavLink>
+      </div>
+      <div className={style.profileContainer}>
+        <h3 className={style.profileInformation}>Personal Information</h3>
+        <div className={style.profileImage}>
+          <img src={avatar} alt={'avatar picture'} />
+          <NavLink to={'/'}>
+            <div className={style.changeProfileImage}>
+              <img src={camera} alt={'change avatar picture'} />
+            </div>
+          </NavLink>
+        </div>
+        <div className={style.changeProfileNameWrapper}>
+          {mode ? (
+            <input
+              value={value}
+              onChange={NameChanger}
+              autoFocus
+              onBlur={() => {
+                setMode(false)
+              }}
+            />
+          ) : (
+            <>
+              <h3 className={style.profileName}>{value}</h3>
+
+              <img
+                src={pencil}
+                alt={'change name'}
+                onClick={() => {
+                  setMode(true)
+                }}
+              />
+            </>
+          )}
+        </div>
+        <div className={style.profileEmail}>
+          <h4>yoyoyo@gmail.com</h4>
+        </div>
+        <div className={style.buttonLogout}>
+          <NavLink to={'/'}>
+            <img src={logout} alt={'log out'} />
+            Log Out
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  )
 }
