dba636cce32ec32c67f1fbf7b2b287c0fb4b2583
Aliaksandr <byshlata@yandex.ru>
2022-08-21 18:12:23 +0300

    start work on tha forgot page

--- src/pages/forgot/Forgot.tsx
+++ src/pages/forgot/Forgot.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+
 import { Title } from 'components/title/Title'
 
 export const Forgot = () => {
   return (
     <>
       <Title text="Forgot your password?" />
+      <input />
     </>
   )
 }
