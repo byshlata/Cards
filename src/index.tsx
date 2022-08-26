dd7f63b0690690bbf109a38064e8a90372025f54
Aliaksandr <byshlata@yandex.ru>
2022-08-26 18:07:30 +0300

    last

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom/client'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import 'index.sass'
 
-import {App} from './app/App'
-import {store} from './store/store'
+import { App } from './app/App'
+import { store } from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement)
 root.render(
