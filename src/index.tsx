d5b09d5511efa5aa011ef5c8a45d3e0a94dd4cf5
Aliaksandr <byshlata@yandex.ru>
2022-08-26 18:12:54 +0300

    gygygy

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import 'index.sass'
 
 import { App } from './app/App'
@@ -9,7 +10,9 @@ import { store } from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement)
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 )
