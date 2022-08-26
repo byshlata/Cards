885e8fc5fa66b4a5afe0c27aee78b90a4aa4940e
Aliaksandr <byshlata@yandex.ru>
2022-08-26 19:54:33 +0300

    hach

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import 'index.sass'
 
+import { BrowserRouter, HashRouter } from 'react-router-dom'
+
 import { App } from './app/App'
 import { store } from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement)
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>
 )
