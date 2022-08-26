8f80562c599bd618fc84937c624d217d7841117d
Aliaksandr <byshlata@yandex.ru>
2022-08-26 18:09:34 +0300

    lalalala

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 import 'index.sass'
 
 import { App } from './app/App'
