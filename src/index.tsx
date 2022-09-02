040e8578081fdc2c9bd01e4a98d82640a5b623fa
Aliaksandr <byshlata@yandex.ru>
2022-09-02 17:37:44 +0300

    two friday commit

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,6 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import 'index.sass'
 
-import { BrowserRouter, HashRouter } from 'react-router-dom'
-
 import { HashRouter } from 'react-router-dom'
 
 import { App } from './app/App'
