61404303c4857f88af323a5b4d3cce75f46fafd6
Aliaksandr <byshlata@yandex.ru>
2022-09-04 22:05:25 +0300

    chage grade

--- src/components/grade/Grade.tsx
+++ src/components/grade/Grade.tsx
@@ -1,49 +1,28 @@
-import React, { FC } from 'react'
+import React, { FC, ReactElement } from 'react'
 
 import style from './Grade.module.sass'
 
-import { fullStar, halfStar, emptyStar } from './index'
+import { emptyStar, fullStar, halfStar } from './index'
+import { createGrade } from 'components/grade/utils/createGrade'
 
 type GradeType = {
   rating: number
 }
+
+export type StarGradeType = {
+  fullStar: ReactElement
+  halfStar: ReactElement
+  emptyStar: ReactElement
+}
+
+const START_GRADE = {
+  fullStar: <img className={style.starWrapper} src={fullStar} alt={'full star'} />,
+  halfStar: <img className={style.starWrapper} src={halfStar} alt={'half star'} />,
+  emptyStar: <img className={style.starWrapper} src={emptyStar} alt={'empty star'} />,
+}
+
 export const Grade: FC<GradeType> = ({ rating }) => {
-  // const divKey = new Date().getMilliseconds() * Math.floor(Math.random() * 100)
-
-  const grader = (rating: number) => {
-    let integer = Math.floor(rating)
-    let fullStarElement = (
-      <div className={style.starWrapper}>
-        <img src={fullStar} alt={'full star'} />
-      </div>
-    )
-    let halfStarElement = (
-      <div className={style.starWrapper}>
-        <img src={halfStar} alt={'half star'} />
-      </div>
-    )
-    let emptyStarElement = (
-      <div className={style.starWrapper}>
-        <img src={emptyStar} alt={'empty star'} />
-      </div>
-    )
-    let emptyArray = []
-
-    for (let i = 0; i < integer; i++) {
-      emptyArray.push(fullStarElement)
-    }
-    let differenceBetweenNumbers = rating - integer
-
-    if (differenceBetweenNumbers > 0) {
-      emptyArray.push(halfStarElement)
-    }
-    while (emptyArray.length < 5) {
-      emptyArray.push(emptyStarElement)
-    }
-
-    return emptyArray
-  }
-  let totalRating = grader(rating)
+  const totalRating = createGrade(rating, START_GRADE)
 
   return (
     <>
