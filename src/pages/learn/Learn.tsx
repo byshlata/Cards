42c2f9ad7a802828e96990cdf62d61f4ccf4dcd1
UserPasha <pasharoma13@gmail.com>
2022-09-08 10:49:25 +0300

    Laern. Part Random

--- src/pages/learn/Learn.tsx
+++ src/pages/learn/Learn.tsx
@@ -19,7 +19,17 @@ export const Learn = () => {
     setIsShow(true)
   }
   //const cards = useSelector=> cards
-
+type testType = {
+  answer: string
+  question: string
+  cardsPack_id: string
+  grade: number
+  shots: number,
+  user_id: string
+  created: string
+  updated: string
+  _id: string
+}
   const testCards = [
     {
       answer: 'no answer',
@@ -30,7 +40,7 @@ export const Learn = () => {
       user_id: '142151531535151',
       created: '2020-05-13T11:05:44.867Z',
       updated: '2020-05-13T11:05:44.867Z',
-      _id: '631756fd5fc57f2a086f51f7',
+      _id: '631756f25fc57f2a086f51f5',
     },
     {
       answer: 'answer1',
@@ -41,7 +51,7 @@ export const Learn = () => {
       user_id: '142151531535151',
       created: '2020-05-13T11:05:44.867Z',
       updated: '2020-05-13T11:05:44.867Z',
-      _id: '5ebbd48876810f1ad0e7ece4',
+      _id: '631756fb5fc57f2a086f51f6',
     },
     {
       answer: '333',
@@ -52,17 +62,37 @@ export const Learn = () => {
       user_id: '142151531535151',
       created: '2020-05-13T11:05:44.867Z',
       updated: '2020-05-13T11:05:44.867Z',
-      _id: '5ebbd48876810f1ad0e7ece5',
+      _id: '631756fd5fc57f2a086f51f7',
+    },
+    {
+      answer: '1+1',
+      question: '1+1',
+      cardsPack_id: '5eb6a2f72f849402d46c6ac4',
+      grade: 1.987525071790364,
+      shots: 1,
+      user_id: '142151531535151',
+      created: '2020-05-13T11:05:44.867Z',
+      updated: '2020-05-13T11:05:44.867Z',
+      _id: '63199db446b5d30818fd1f1c',
     },
   ]
-  const [cards, setCards] = useState(testCards[0])
+ 
 
   const inputValues2 = ['Did not know', 'Forgot', 'A lot of thought', 'Confused', 'Knew the answer']
 
-  //const grades = [1,2,3,4,5]
-
+ const cardsRandom = (arr: testType[]):testType =>{
+   let a = []
+   let c:number[] = arr.map((a)=>+(60-a.grade*10).toFixed(1))
+   for(let i=0; i<c.length; i++){
+     for(let j=0; j<c[i]; j++)
+       a.push(arr[i])
+   }
+   let b = a.sort((a,b)=>a.grade-b.grade)
+   return b[Math.floor(Math.random() *b.length)]
+ }
+  const [cards, setCards] = useState(cardsRandom(testCards))
   const cardChanger = (id: string) => {
-    setCards(testCards[2])
+    setCards(cardsRandom(testCards))
 
     const convertValueToGrade = (value: string): number => {
       let grade
